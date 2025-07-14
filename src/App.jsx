838a28a8165383743bbed25fc59b1f8c897a243f
talhagun08 <guntalha@gmail.com>
2025-07-14 10:52:45 +0300

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import {
+  PlusCircle,
+  Calendar,
+  Search,
+  Moon,
+  Sun,
+  Edit2,
+  Trash2,
+} from "lucide-react";
+import "./App.css";
+
+function DiaryApp() {
+  return <div>Hello</div>;
+}
+
+export default DiaryApp;
